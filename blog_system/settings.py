1e95754d650d8bc7fe9fce5f31aee9f9275866bd
Mykhailo Doroshenko <doroshenko.misha73@gmail.com>
2025-03-22 14:31:47 +0100

    Update settings for deployment

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p)6ul8l5v+9ci0ck_9q5#oy*ugl==&lpkt7pz4=3)2$ddpwolz"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-p)6ul8l5v+9ci0ck_9q5#oy*ugl==&lpkt7pz4=3)2$ddpwolz")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
